fa5bd342dd7ccde4283b326563eeab1781781298
김근호 <kimkgh8@gmail.com>
2023-03-06 00:51:53 +0900

    💄 Style : chatGPT한테 업그레이드 요청 해서 얻은 결과..

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 
-input.addEventListener("keydown", (event) => {
-	if (event.key === "Enter") {
-		addTask(input.value);
-		input.value = "";
+// added form submit event listener
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const task = input.value.trim();
+	if (task !== '') {
+		addTask(task);
+		input.value = '';
+	} else {
+		showError('Please enter a task');
 	}
 });
+
+// function to show error message
+function showError(message) {
+	const error = document.createElement('div');
+	error.id = 'error';
+	error.innerText = message;
+	form.insertBefore(error, input.nextSibling);
+	setTimeout(() => {
+		error.remove();
+	}, 3000);
+}
 
 function addTask(task) {
 	const item = document.createElement("li");
@@ -21,13 +38,15 @@ function addTask(task) {
 	item.appendChild(deleteButton);
 	list.appendChild(item);
 
+	// added animation classes to move up and down
+	item.classList.add('move-up');
 	setTimeout(() => {
-		item.classList.add("fade");
-	}, 0);
-
-	item.addEventListener("transitionend", () => {
-		item.classList.remove("fade");
-	});
+		item.classList.remove('move-up');
+		item.classList.add('move-down');
+	}, 100);
+	setTimeout(() => {
+		item.classList.remove('move-down');
+	}, 400);
 
 	item.addEventListener("click", () => {
 		item.classList.toggle("done");
@@ -35,9 +54,14 @@ function addTask(task) {
 }
 
 function removeTask(item) {
-	item.classList.add("fade");
-
-	item.addEventListener("transitionend", () => {
+	// added animation classes to move up and down
+	item.classList.add('move-up');
+	setTimeout(() => {
+		item.classList.remove('move-up');
+		item.classList.add('move-down');
+	}, 100);
+	setTimeout(() => {
+		item.classList.remove('move-down');
 		item.remove();
-	});
+	}, 400);
 }
