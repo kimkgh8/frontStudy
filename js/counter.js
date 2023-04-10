82b5e5ffc6ee448bc40684301bef18b6c165629b
김근호 <kimkgh8@gmail.com>
2023-04-10 23:10:29 +0900

    ✨ Feat : 양수 음수 판단하여 색상 변경되도록 수정

--- js/counter.js
+++ js/counter.js
@@ -5,14 +5,35 @@ let increaseBtn = document.getElementById("increaseBtn");
 decreaseBtn.addEventListener("click", function() {
 	let cnt = document.querySelector(".count");
 	cnt.textContent = Number(cnt.textContent) - 1;
+	changeFontColor(cnt.textContent);
 });
 
 resetBtn.addEventListener("click", function() {
 	let cnt = document.querySelector(".count");
 	cnt.textContent = 0;
+	changeFontColor(cnt.textContent);
 });
 
 increaseBtn.addEventListener("click", function() {
 	let cnt = document.querySelector(".count");
 	cnt.textContent = Number(cnt.textContent) + 1;
+	changeFontColor(cnt.textContent);
 })
+
+const changeFontColor = (target) => {
+	let count = document.querySelector(".count");
+	clearFontColor(count);
+	if (target < 0 ) {
+		count.classList.add("font-minus");
+	} else if (target > 0) {
+		count.classList.add("font-plus");
+	} else {
+		count.classList.add("font-zero");
+	}
+}
+
+const clearFontColor = (target) => {
+	target.classList.remove("font-minus");
+	target.classList.remove("font-plus");
+	target.classList.remove("font-zero");
+}
